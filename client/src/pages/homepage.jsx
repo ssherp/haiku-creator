bb21c6ee144d1e66e5a82bf8d9ccca08592d9cc8
Chris L <chris@larsonwerks.com>
2023-09-09 13:10:33 -0700

    added pages files

---
+++ client/src/pages/homepage.jsx
@@ -0,0 +1,26 @@
+<div class="container"><!--parent container to rule them all-->
+        <header class="pure-g"><!--header section-->
+            <div class="pure-u-2-3">
+                <h1>HAIKU HELPER</h1>
+            </div>
+            <nav class="pure-u-1-3">
+                <ul>
+                    <li><a href="#">HOME</a></li>
+                    <li><a href="#">SIGN-UP/LOGIN</a></li>
+                    <li><a href="#">PROFILE</a></li>
+                </ul>
+                <a href="#"><span class="creator">CREATE HAIKU</span></a>
+            </nav>
+        </header><!--end header section-->
+        <main class="pure-g"><!--main section for haiku and introductory content-->
+            <div class="pure-u-2-3">
+                <p class="haiku" id="first_line">Whispers of the wind</p>   
+                <p class="haiku" id="second_line">Leaves dancing in golden light,</p>
+                <p class="haiku" id="third_line">Nature's song begins</p> 
+            </div>
+            <div class="pure-u-1-3">
+                <p><b>Haiku Helper:</b> Your Gateway to Poetic Bliss! Unleash your inner poet with ease using our intuitive platform. Craft beautiful haikus effortlessly, guided by our user-friendly drag-n-drop interface. Dive into a world of syllabic harmony and nature-inspired verse. Let your creativity flourish with <b>Haiku Helper</b>! Start composing today.</p>
+            </div>
+        </main><!--end main section-->
+        <aside class="unsplash"></aside><!--container for auto generated randomized unsplash image - set as a background image in our CSS file-->
+    </div> <!--closes container-->
