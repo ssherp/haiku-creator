f58b7e238e6678d58df77f5eb827389ce48e3fe5
Chris L <chris@larsonwerks.com>
2023-09-09 13:11:25 -0700

    added jsx to login page

---
+++ client/src/pages/login.jsx
@@ -0,0 +1,55 @@
+<div class="container"><!--parent container to rule them all-->
+        <header class="pure-g"><!--header section-->
+            <div class="pure-u-2-3">
+                <h1>HAIKU HELPER</h1>
+            </div>
+            <nav class="pure-u-1-3">
+                <ul>
+                    <li><a href="#">HOME</a></li>
+                    <li><a href="#">SIGN-UP/LOGIN</a></li>
+                    <!--li><a href="#">PROFILE</a></li -->
+                </ul>
+                <!-- a href="#"><span class="creator">CREATE HAIKU</span></a -->
+            </nav>
+        </header><!--end header section-->
+        <main class="pure-g"><!--main section for haiku and introductory content-->
+            <div class="pure-u-1-2 login">
+            </div>
+            <div class="pure-u-1-5">
+                <form>
+                    <h2>LOGIN</h2>
+                    <div>
+                        <label for="username">Username:</label><br>
+                        <input type="text" id="username" name="username" required>
+                    </div>
+                    <div>
+                        <label for="password">Password:</label><br>
+                        <input type="password" id="password" name="password" required>
+                    </div>
+                    <div>
+                        <button type="submit">LOGIN</button>
+                    </div>
+                </form>
+            </div>
+            <div class="pure-u-1-5 sign">
+                <form>
+                    <h2>SIGN-UP</h2>
+                    <div>
+                        <label for="username">Username:</label><br>
+                        <input type="text" id="username" name="username" required>
+                    </div>
+                    <div>
+                        <label for="username">Email:</label><br>
+                        <input type="email" id="email" name="email" required>
+                    </div>
+                    <div>
+                        <label for="password">Password:</label><br>
+                        <input type="password" id="password" name="password" required>
+                    </div>
+                    <div>
+                        <button type="submit">SIGN-UP</button>
+                    </div>
+                </form>
+            </div>
+        </main><!--end main section-->
+    </div> <!--closes container--></br>
