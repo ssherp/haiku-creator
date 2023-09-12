5b0583872daa292cde40557220364427411a1bc2
Chris L <chris@larsonwerks.com>
2023-09-11 19:08:48 -0700

    updated Creator.jsx

--- client/src/pages/Creator.jsx
+++ client/src/pages/Creator.jsx
@@ -1,67 +1,12 @@
-<div class="container"><!--parent container to rule them all-->
-    <Nav />
-    <main class="pure-g">
-        <div class="pure-u-2-3">
-            <p class="haiku" id="first_line">Whispers of the wind</p>   
-            <p class="haiku" id="second_line">Leaves dancing in golden light,</p>
-            <p class="haiku" id="third_line">Nature's song begins</p> 
-        </div>
-        <div class="pure-u-1-3"><!--move the save haiku button out of the nav and into this div>
-            <span className="creator">CREATE HAIKU</span>
-        </div>
-    </main>
-    <aside class="pure-g">
-        <div class="pure-u-2-3 wordDeck">
-            <div>
-                <h3>1 syllable</h3>
-                <p class="words">Leaf<br>
-                    Stream<br>
-                    Sun<br>
-                    Tree<br>
-                    Sky</p>
-            </div>
-            <div>
-                <h3>2 syllables</h3>
-                <p class="words">Meadow<br>
-                    Whisper<br>
-                    Pebble<br>
-                    Rainbow<br>
-                    Blossom</p>
-            </div>
-            <div>
-                <h3>3 syllables</h3>
-                <p class="words">Butterfly<br>
-                    Waterfall<br>
-                    Firefly<br>
-                    Horizon<br>
-                    Tranquil</p>
-            </div>
-            <div>
-                <h3>Verbs</h3>
-                <p class="words">Whispers<br>
-                    Dances<br>
-                    Glistens<br>
-                    Sways<br>
-                    Blooms</p>
-            </div>
-            <div>
-                <h3>Adjectives</h3>
-                <p class="words">Gentle<br>
-                    Tranquil<br>
-                    Radiant<br>
-                    Fragrant<br>
-                    Serene</p>
-            </div>
-            <div>
-                <h3>Prepositions</h3>
-                <p class="words">of<br>
-                    in<br>
-                    for<br>
-                    over<br>
-                    on</p>
-            </div>
+import Nav from "../components/Nav";
+import Assemble from "../components/Assemble";
+import Words from "../components/Words";
 
-        </div>
-        <div class="pure-u-1-3 inspire"></div>
-    </aside>
+const Creator = () => {
+<div className="container">
+    <Nav />
+    <Assemble />
+    <Words />
 </div>
+}
+export default Creator;
