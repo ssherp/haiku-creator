4c2e1e92cb43673d0a76ba89bd5edf3c1f928742
ssherp <ssherpa89@gmail.com>
2023-09-12 11:58:52 -0700

    Haiku models

--- server/models/User.js
+++ server/models/User.js
@@ -1,4 +1,6 @@
-const { Schema, model } = require('mongoose');
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
 const bcrypt = require('bcrypt');
 
 const userSchema = new Schema({
