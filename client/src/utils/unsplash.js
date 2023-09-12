dc8165790bbf8450b4f7f307144e1d455dc7740f
mattschneble <136631410+mattschneble@users.noreply.github.com>
2023-09-12 11:09:21 -0700

    added unsplash.js to utils

---
+++ client/src/utils/unsplash.js
@@ -0,0 +1,34 @@
+// import react 
+import React from 'react'; 
+
+// set up API call to unsplash to generate nature image
+function unsplash() {
+    var apiKey = "tOQT-14Jo3wp5-I1CMKVmCdvQXVRihQoi8pJ7AsiPQI";
+    var apiKey2 = "Ab4F25pH3_s49oNWOzNXoahqu18przepQm1JgDMKkZA";
+    var apiKey3 = "oeF6_nedrYDZgPYD3W22C9NSsJsCa0DeZfWTlHO7u2I";
+    var apiKey4 = "BhBNA4hLuZrHL_xWMeD4BgR-aMZgW07kKJhE4iDhk7E";
+  
+    var requestURL =
+      "https://api.unsplash.com/photos/random?query=nature?orientation=landscape&client_id=" +
+      apiKey;
+  
+    fetch(requestURL)
+      .then((response) => response.json())
+      .then((data) => {
+        var photoUrl = data.urls.regular;
+  
+        // Create an <img> element and set its source to the random photo URL
+        document.body.setAttribute(
+          "style",
+          "background-image:url(" + photoUrl + ")"
+        );
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  }
+  
+unsplash();
+
+// export for use
+export default unsplash;
