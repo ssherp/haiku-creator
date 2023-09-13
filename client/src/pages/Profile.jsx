d486f8c2c6e16437cd3b8a966525204e5ea9472f
hyunghung <97567582+hyunghung@users.noreply.github.com>
2023-09-13 03:21:28 -0700

    Hypothetical profile page that should work if we logged in

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,32 +1,63 @@
 import { Navigate, useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
+import { useState, useEffect } from 'react';
 import unsplash from '../utils/unsplash';
 
-// import { QUERY_USER, QUERY_ME } from '../utils/queries';
+import { USER } from '../utils/queries'; // Import the correct query
 
-// import Auth from '../utils/auth';
+import Auth from '../utils/auth';
 import Nav from "../components/Nav";
 import Cards from "../components/Cards";
 
 const Profile = () => {
-  unsplash()
-  // const { username: userParam } = useParams();
+  const [unsplashDataLoaded] = useState(false);
+  useEffect(() => {
+    unsplash()
+      .then(() => {
+        // Handle the successful loading of the image if needed
+      })
+      .catch((error) => {
+        console.error('Error loading unsplash data:', error);
+      });
+  }, []);
 
-  // const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
+  const { username: userParam } = useParams();
+
+ // FOR TESTING PURPOSES PLEASE FORGIVE ME MY SIGNUP AND LOGIN DONT WORK YET GUYS 
+ const mockUser = {
+  username: "testuser",
+  email: "testuser@example.com",
+  haikus: [
+    {
+      _id: "1",
+      content: "Haiku 1 Content",
+    },
+    {
+      _id: "2",
+      content: "Haiku 2 Content",
+    },
+  ],
+};
+
+  const user = mockUser;
+
+  // const { loading, data, error } = useQuery(userParam ? USER : USER, { 
   //   variables: { username: userParam },
   // });
 
-  // const user = data?.me || data?.user || {};
-  // if (
-  //   Auth.loggedIn() && 
-  //   /* Run the getProfile() method to get access to the unencrypted token value in order to retrieve the user's username, and compare it to the userParam variable */
-  //   Auth.getProfile().authenticatedPerson.username === userParam
-  // ) {
+  // const user = data?.user || {};
+
+  // if (Auth.loggedIn() && Auth.getProfile().authenticatedPerson.username === userParam) {
   //   return <Navigate to="/profile" />;
   // }
 
   // if (loading) {
   //   return <div>Loading...</div>;
+  // }
+
+  // if (error) { 
+  //   console.error('Error fetching user data:', error);
+  //   return <div>Error fetching user data.</div>;
   // }
 
   // if (!user?.username) {
@@ -40,9 +71,34 @@ const Profile = () => {
 
   return (
     <div className="container">
-        <Nav />
-        <Cards />
-        <aside className="unsplash" id="haikuPicture"></aside>
+      <Nav />
+      <Cards />
+      <aside className="unsplash" id="haikuPicture"></aside>
+      <div
+        style={{
+          backgroundImage: `url(${unsplashDataLoaded ? unsplash() : ''})`,
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+          height: '100%',
+        }}
+      ></div>
+      <div className="user-profile">
+        <h2>User Profile</h2>
+        <p>
+          Username: {user.username}
+        </p>
+        <p>
+          Email: {user.email}
+        </p>
+      </div>
+      <div className="user-haikus">
+        <h2> User's Haikus </h2>
+        <ul>
+          {user.haikus.map((haiku) => (
+            <li key={haiku._id}>{haiku.content}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
