21a37c23fb9f7afcda5137810944b90a8c68533d
Chris L <chris@larsonwerks.com>
2023-09-13 13:30:48 -0700

    updated the profile page UX

--- client/src/components/Cards/index.jsx
+++ client/src/components/Cards/index.jsx
@@ -1,16 +1,41 @@
 import React from 'react';
 
+// FOR TESTING PURPOSES PLEASE FORGIVE ME MY SIGNUP AND LOGIN DONT WORK YET GUYS 
+const mockUser = {
+  username: "testuser",
+  email: "testuser@example.com",
+  haikus: [
+    {
+      _id: "1",
+      content: "Whispers of the wind...",
+    },
+    {
+      _id: "3",
+      content: "Autumn leaves take flight...",
+    },
+    {
+      _id: "4",
+      content: "Morning mist descends...",
+    },
+    {
+      _id: "5",
+      content: "Moonlight paints the sea...",
+    },
+    {
+      _id: "6",
+      content: "Sun-lit petals sway...",
+    },
+  ],
+};
+
+  const user = mockUser;
+
 function HaikuSection() {
   return (
       <ul className="pure-u-2-3 cardDeck">
-        <li className="card">Whispers of the wind...</li>
-        <li className="card">Whispers of the wind...</li>
-        <li className="card">Whispers of the wind...</li>
-        <li className="card">Whispers of the wind...</li>
-        <li className="card">Whispers of the wind...</li>
-        <li className="card">Whispers of the wind...</li>
-        <li className="card">Whispers of the wind...</li>
-        <li className="card">Whispers of the wind...</li>
+        {user.haikus.map((haiku) => (
+                <li className="card" key={haiku._id}>{haiku.content}</li>
+              ))}
       </ul>
 
   );
