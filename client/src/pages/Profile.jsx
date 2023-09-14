dff288a5886e077b98b10c58dd1fc598fca75cbf
mattschneble <136631410+mattschneble@users.noreply.github.com>
2023-09-13 22:17:59 -0700

    debugged profile page redirect issue

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -2,6 +2,8 @@ import { Navigate, useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
 import { useState, useEffect } from 'react';
 import unsplash from '../utils/unsplash';
+import { ADD_USER } from '../utils/mutations';
+import { useMutation } from '@apollo/client';
 
 import { USER } from '../utils/queries'; // Import the correct query
 
@@ -10,8 +12,34 @@ import Nav from "../components/Nav";
 import Cards from "../components/Cards";
 
 const Profile = () => {
+  const {loading, error, data} = useQuery(USER)
+  const { userInfo } = data || {}
+  const [userFormData, setUserFormData] = useState({
+    username: '',
+    email: '',
+    password: '',
+  })
+
+  const [addUser] = useMutation(ADD_USER);
   const [unsplashDataLoaded] = useState(false);
   useEffect(() => {
+    const handleInputChange = (event) => {
+      const {name, value} = event.target;
+      setUserFormData({...userFormData, [name]:value})
+    }
+    const handleFormSubmit = async (event) => {
+      event.preventDefault();
+      const form = event.currentTarget;
+      try {
+        const {data} = await addUser ({
+          variables: {...userFormData}
+        })
+        Auth.login(data.addUser.token)
+      } catch (err) {
+        console.log(err)
+      }
+    }
+
     unsplash()
       .then(() => {
         // Handle the successful loading of the image if needed
@@ -23,23 +51,23 @@ const Profile = () => {
 
   const { username: userParam } = useParams();
 
- // FOR TESTING PURPOSES PLEASE FORGIVE ME MY SIGNUP AND LOGIN DONT WORK YET GUYS 
- const mockUser = {
-  username: "testuser",
-  email: "testuser@example.com",
-  haikus: [
-    {
-      _id: "1",
-      content: "Haiku 1 Content",
-    },
-    {
-      _id: "2",
-      content: "Haiku 2 Content",
-    },
-  ],
-};
+//  // FOR TESTING PURPOSES PLEASE FORGIVE ME MY SIGNUP AND LOGIN DONT WORK YET GUYS 
+//  const mockUser = {
+//   username: "testuser",
+//   email: "testuser@example.com",
+//   haikus: [
+//     {
+//       _id: "1",
+//       content: "Haiku 1 Content",
+//     },
+//     {
+//       _id: "2",
+//       content: "Haiku 2 Content",
+//     },
+//   ],
+// };
 
-  const user = mockUser;
+//   const user = mockUser;
 
   // const { loading, data, error } = useQuery(userParam ? USER : USER, { 
   //   variables: { username: userParam },
@@ -78,10 +106,11 @@ const Profile = () => {
           <div className="user-profile">
             <h2>User Profile</h2>
             <p>
-              Username: {user.username}
+              Username: {userInfo?.username}
+              {console.log(USER)}
             </p>
             <p>
-              Email: {user.email}
+              {/* Email: {userInfo.email} */}
             </p>
           </div>
           {/* <div className="user-haikus">
